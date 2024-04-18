25724c09e5412876d70ff5685fb8363d63d7a3a7
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2024-04-18 16:20:47 -0400

    Code now working

--- index.js
+++ index.js
@@ -11,49 +11,70 @@ const issuesRepoName = context.payload?.repository?.name || process.env.ISSUES_R
 const issuesNumber = context.payload?.issue?.number || process.env.ISSUES_NUMBER;
 
 // Get the repository name to create and template from the workflow file
-const createRepoName = core.getInput('repo_name') || process.env.REPO_NAME;
-const repoTemplate = core.getInput('repo_template') || process.env.REPO_TEMPLATE;
+const createRepoName = core.getInput("repo_name") || process.env.REPO_NAME;
+const repoTemplate = core.getInput("repo_template") || process.env.REPO_TEMPLATE;
 
 // Get the authorization inputs from the workflow file
-const githubAppId = core.getInput('app_id') || process.env.APP_ID;
-const githubAppPrivateKey = core.getInput('app_private_key') || process.env.APP_PRIVATE_KEY;
-const githubAppInstallationId = core.getInput('app_installation_id') || process.env.APP_INSTALLATION_ID;
-const githubApiUrl = core.getInput('api_url') || process.env.API_URL;
-const githubPAT = core.getInput('pat') || process.env.PAT;
-const isDebug = core.getInput('is_debug') || process.env.DEBUG;
+const githubApiUrl = core.getInput("api_url") || process.env.API_URL;
+const githubPAT = core.getInput("pat") || process.env.PAT;
+const isDebug = core.getInput("is_debug") || process.env.DEBUG;
 
 core.info(`isDebug? ${isDebug}`);
 
 // Create Octokit instances for source and target
-const octokit = createOctokitInstance(
-  githubPAT,
-  githubAppId,
-  githubAppPrivateKey,
-  githubAppInstallationId,
-  githubApiUrl
-);
+const octokit = createOctokitInstance(githubPAT, githubApiUrl);
 
 // Function to create Octokit instance
-function createOctokitInstance(PAT, appId, appPrivateKey, appInstallationId, apiUrl) {
+function createOctokitInstance(PAT, apiUrl) {
   // Prefer PAT over GitHub App for authentication
-  if (PAT) {
-    return new Octokit({
-      auth: PAT,
-      baseUrl: apiUrl,
-      log: core.isDebug() ? console : null,
-    });
-  } else {
-    return new Octokit({
-      authStrategy: createAppAuth,
-      auth: {
-        appId: appId,
-        privateKey: appPrivateKey,
-        installationId: appInstallationId,
-      },
-      baseUrl: apiUrl,
-      log: core.isDebug() ? console : null,
-    });
-  }
+  return new Octokit({
+    auth: PAT,
+    baseUrl: apiUrl,
+    log: core.isDebug() ? console : null,
+  });
+}
+
+async function createComment(body) {
+  return await octokit.issues.createComment({
+    owner: organization,
+    repo: issuesRepoName,
+    issue_number: issuesNumber,
+    body: body,
+  });
+}
+
+async function createRepo() {
+  return await octokit.request("POST /repos/{template_owner}/{template_repo}/generate", {
+    template_owner: organization,
+    template_repo: repoTemplate,
+    owner: organization,
+    name: createRepoName,
+    include_all_branches: true,
+    private: true,
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+}
+async function addCollaborator(username) {
+  await octokit.request("PUT /repos/{owner}/{repo}/collaborators/{username}", {
+    owner: organization,
+    repo: createRepoName,
+    username: username,
+    permission: "admin",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+}
+
+async function closeIssue() {
+  return await octokit.issues.update({
+    owner: organization,
+    repo: issuesRepoName,
+    issue_number: issuesNumber,
+    state: "closed",
+  });
 }
 
 // Create a new repository from the template, using the Octokit instance,
@@ -62,73 +83,50 @@ function createOctokitInstance(PAT, appId, appPrivateKey, appInstallationId, api
 // close the issue
 
 async function createRepoFromTemplate() {
+  core.debug(`
+  vars:
+    user: ${user}
+    organization: ${organization}
+    issuesRepoName: ${issuesRepoName}
+    issueNumber: ${issuesNumber}
+    createRepoName: ${createRepoName}
+    repoTemplate: ${repoTemplate}
+    githubApiUrl: ${githubApiUrl}
+    githubPAT: ${githubPAT}
+  `);
   try {
-    // Check if the repository already exists
-    try {
-      await octokit.repos.get({
-        owner: organization,
-        repo: createRepoName,
-      });
-      core.info(`Repository ${repo} already exists`);
-
-      // Comment back to the issue that repository already exists
-      await octokit.issues.createComment({
-        owner: organization,
-        repo: IssuesRepoName,
-        issue_number: context.payload.issue.number,
-        body: `Repository ${repo} already exists.`
-      });
-      return;
-    } catch (error) {
-      if (error.status !== 404) {
-        throw error;
-      }
-      // Repository does not exist, continue with creation
-    }
-
-    const response = await  octokit.request('POST /repos/{template_owner}/{template_repo}/generate',{
-      template_owner: organization,
-      template_repo: repoTemplate,
-      owner: organization,
-      name: createRepoName,
-      include_all_branches: true,
-      'private': true,
-    });
-    core.info(`Repository created: ${response.data.full_name}`);
-
-    await octokit.repos.addCollaborator({
+    await octokit.repos.get({
       owner: organization,
       repo: createRepoName,
-      username: user,
-      permission: 'admin',
     });
-    core.info(`User ${user} added as an admin to ${repo}`);
+  } catch (error) {
+    if (error.status !== 404) {
+      core.info(`Repository ${createRepoName} already exists`);
+      // Comment back to the issue that repository already exists
+      await createComment(`Repository ${createRepoName} already exists.`);
+      throw error;
+    }
+  }
+
+  try {
+    // Check if the repository already exists
+    const response = await createRepo();
+    core.info(`Repository created: ${response.data.full_name}`);
+
+    await addCollaborator(user);
+    core.info(`User @${user} added as an admin to ${createRepoName}`);
 
     // Comment back to the issue on success
-    await octokit.issues.createComment({
-      owner: organization,
-      repo: IssuesRepoName,
-      issue_number: issuesNumber,
-      body: `Repository created successfully: ${response.data.html_url}\nUser ${user} added as an admin to ${createRepoName}`
-    });
+    await createComment(
+      `Repository created successfully: ${response.data.html_url}\nUser @${user} added as an admin to \`${createRepoName}\``
+    );
 
     // Close the issue on success
-    await octokit.issues.update({
-      owner: organization,
-      repo: IssuesRepoName,
-      issue_number: issuesNumber,
-      state: 'closed'
-    });
+    await closeIssue();
   } catch (error) {
     core.setFailed(error.message);
 
-    // Comment back to the issue on failure
-    await octokit.issues.createComment({
-      owner: organization,
-      repo: issuesRepoName,
-      issue_number: issuesNumber,
-      body: `Failed to create repository ${createRepoName}: ${error.message}`
-    });
+    await createComment(`Failed to create repository ${createRepoName}: ${error.message}`);
   }
 }
 
